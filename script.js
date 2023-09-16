e55e856742f9c85324c2432790c95cb517d76f70
minitree <egle.lunyte@gmail.com>
2023-09-16 20:36:52 +0300

    Expansions for lakes and rivers added

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rollButton = document.getElementById("rollButton");
     const resultElement = document.getElementById("result");
+    const enableExtraDiceLakeCheckbox = document.getElementById("enableExtraDiceLakes");
+    const enableExtraDiceRiverCheckbox = document.getElementById("enableExtraDiceRivers");
     const dieImages = [
         document.getElementById("dieImage1"),
         document.getElementById("dieImage2"),
         document.getElementById("dieImage3"),
-        document.getElementById("dieImage4"), // Add the fourth image element
+        document.getElementById("dieImage4"),
+        document.getElementById("dieImage5"),
+        document.getElementById("dieImage6"),
+        document.getElementById("dieImage7"),
+        document.getElementById("dieImage8"),
     ];
 
     rollButton.addEventListener("click", function () {
+        const BothChecked = enableExtraDiceLakeCheckbox.checked & enableExtraDiceRiverCheckbox.checked ? 8 : 4
+        const numDiceLake = enableExtraDiceLakeCheckbox.checked ? 6 : 4;
+        const numDiceRiver = enableExtraDiceRiverCheckbox.checked ? 6 : 4;
+        const numDice = Math.max(BothChecked, numDiceLake, numDiceRiver)
         const rollResults = [];
-        for (let i = 0; i < 4; i++) { // Change to 4 for the fourth type of dice
-            const randomNumber = Math.floor(Math.random() * 6) + 1; // Adjust to your new dice type
-            const imagePath = `images/die${i + 1}-${randomNumber}.jpg`; // Adjust the image name pattern
+        for (let i = 0; i < numDice; i++) {
+            const randomNumber = Math.floor(Math.random() * 6) + 1;
+            const imagePath = `images/die${i + 1}-${randomNumber}.jpg`;
             dieImages[i].src = imagePath;
             rollResults.push(randomNumber);
         }
